2db64287bef67dec8b280ecf15a3e5ae53a6ee7c
Dillon Christensen <j.dillon.christensen@gmail.com>
2017-07-03 10:53:23 -0500

    made the budget/paycheck in the channel :)

--- budget/main.js
+++ budget/main.js
@@ -17,50 +17,52 @@ function reply(b, m, text) {
 module.exports = (controller, paycheckInstance) => {
     budget.paycheck = paycheckInstance;
 
-    controller.on('direct_message', (b, m) => {
-        const message = m.text.trim();
-        if (is.budget(message)) {
-            const action = message.substr(7);
-            const uId = m.user;
+    controller.on('direct_message,direct_mention,ambient', (b, m) => {
+        if (m.event !== 'ambient' || m.channel === 'G5JA6R84V') {
+            const message = m.text.trim();
+            if (is.budget(message)) {
+                const action = message.substr(7);
+                const uId = m.user;
 
-            if (is.help(action)) {
-                const helpText = [
-                    '`budget balance`\ngets balance and transactions',
-                    '`budget bought {name}, {price}`\nadd transaction with a short `name` and the `price`',
-                    '`budget canbuy {amount}`\nchecks if you can afford to spend amount'
-                ].join('\n\n');
-                b.reply(m, helpText);
-            } else if (is.balance(action)) {
-                budget.balance(uId)
-                    .then(({bal, trans}) => {
-                        const title = `Your balance is *$${bal}*`;
-                        const fields = trans.reverse().map(t => ({
-                            short: false,
-                            title: t.description,
-                            value: `- $${t.price.toFixed(2)}`
-                        }));
-                        b.reply(m, {
-                            text: title,
-                            attachments: [
-                                {
-                                    fallback: title,
-                                    color: bal >= 20 ? '#36a64f' : bal >= 1 ? '#FECC43' : '#FF0000',
-                                    fields: fields
-                                }
-                            ]
-                        });
-                });
-            } else if (is.bought(action)) {
-                budget.bought(uId, ...action.substr(7).split(','))
-                    .then(({bal}) => reply(b, m, `You have $${bal} left`))
-                    .catch(err => reply(b, m, err));
-            } else if (is.canBuy(action)) {
-                const amount = action.match(/\d+\.?\d+/);
-                if (amount && amount.length > 0) {
-                    budget.canBuy(uId, amount[0])
-                        .then(a => reply(b, m, !!a ? 'You can do it!' : 'Not enough funds.'));
-                } else {
-                    reply(b, m, 'I don\'t see the price in this message...');
+                if (is.help(action)) {
+                    const helpText = [
+                        '`budget balance`\ngets balance and transactions',
+                        '`budget bought {name}, {price}`\nadd transaction with a short `name` and the `price`',
+                        '`budget canbuy {amount}`\nchecks if you can afford to spend amount'
+                    ].join('\n\n');
+                    b.reply(m, helpText);
+                } else if (is.balance(action)) {
+                    budget.balance(uId)
+                        .then(({bal, trans}) => {
+                            const title = `Your balance is *$${bal}*`;
+                            const fields = trans.reverse().map(t => ({
+                                short: false,
+                                title: t.description,
+                                value: `- $${t.price.toFixed(2)}`
+                            }));
+                            b.reply(m, {
+                                text: title,
+                                attachments: [
+                                    {
+                                        fallback: title,
+                                        color: bal >= 20 ? '#36a64f' : bal >= 1 ? '#FECC43' : '#FF0000',
+                                        fields: fields
+                                    }
+                                ]
+                            });
+                    });
+                } else if (is.bought(action)) {
+                    budget.bought(uId, ...action.substr(7).split(','))
+                        .then(({bal}) => reply(b, m, `You have $${bal} left`))
+                        .catch(err => reply(b, m, err));
+                } else if (is.canBuy(action)) {
+                    const amount = action.match(/\d+\.?\d+/);
+                    if (amount && amount.length > 0) {
+                        budget.canBuy(uId, amount[0])
+                            .then(a => reply(b, m, !!a ? 'You can do it!' : 'Not enough funds.'));
+                    } else {
+                        reply(b, m, 'I don\'t see the price in this message...');
+                    }
                 }
             }
         }
--- paycheck/main.js
+++ paycheck/main.js
@@ -18,28 +18,30 @@ module.exports = controller => {
     //    TODO: don't hack this way
     (require('../budget/main'))(controller, paycheck);
 
-    controller.on('direct_message', (b, m) => {
-        const message = m.text.trim();
-        if (is.module(message)) {
-            const action = message.substr(9);
+    controller.on('direct_mention,ambient', (b, m) => {
+        if (m.event !== 'ambient' || m.channel === 'G5JA6R84V') {
+            const message = m.text.trim();
+            if (is.module(message)) {
+                const action = message.substr(9);
 
-            if (is.help(action)) {
-                const helpText = [
-                    '`paycheck balance`\ngets remaining balance',
-                    '`paycheck pay {amount}`\nadd transaction with just a dollar amount',
-                    '`paycheck reset {paycheck total}`\nresets paycheck balance and budgets\n`paycheck total` is optional but can be used to reset beginning of paycheck balance to specific amount otherwise it will default to $2656.65'
-                ].join('\n\n');
-                b.reply(m, helpText);
-            } else if (is.balance(action)) {
-                paycheck.balance()
-                    .then(bal => reply(b, m, `You have $${bal}`));
-            } else if (is.bought(action)) {
-                paycheck.pay(action.substr(4))
-                    .then(res => reply(b, m, `You have $${res} left`))
-                    .catch(err => reply(b, m, err));
-            } else if (is.reset(action)) {
-                paycheck.reset(action.substr(6))
-                    .then(bal => reply(b, m, `Budget reset to $${bal} :+1:`));
+                if (is.help(action)) {
+                    const helpText = [
+                        '`paycheck balance`\ngets remaining balance',
+                        '`paycheck pay {amount}`\nadd transaction with just a dollar amount',
+                        '`paycheck reset {paycheck total}`\nresets paycheck balance and budgets\n`paycheck total` is optional but can be used to reset beginning of paycheck balance to specific amount otherwise it will default to $2656.65'
+                    ].join('\n\n');
+                    b.reply(m, helpText);
+                } else if (is.balance(action)) {
+                    paycheck.balance()
+                        .then(bal => reply(b, m, `You have $${bal}`));
+                } else if (is.bought(action)) {
+                    paycheck.pay(action.substr(4))
+                        .then(res => reply(b, m, `You have $${res} left`))
+                        .catch(err => reply(b, m, err));
+                } else if (is.reset(action)) {
+                    paycheck.reset(action.substr(6))
+                        .then(bal => reply(b, m, `Budget reset to $${bal} :+1:`));
+                }
             }
         }
     });
