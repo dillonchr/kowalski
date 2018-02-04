5c733cf2a435b226f3626884e9d0d7d1cceeca79
Dillon Christensen <j.dillon.christensen@gmail.com>
2018-02-04 01:05:42 -0600

    initial stab at using funhouse for kowalski

--- budget/main.js
+++ budget/main.js
@@ -1,27 +1,12 @@
-const budget = require('./budget');
+const { budget } = require('funhouse-client');
 
 const reply = (b, m, text) => b.reply(m, {response_type: 'ephemeral', text});
 
 const respondWithBalance = (b, m, userId) => {
-    budget.balance(userId)
-        .then(({bal, trans}) => {
-            const text = `Your balance is *$${bal}*`;
-            b.reply(m, {
-                text,
-                attachments: [
-                    {
-                        fallback: text,
-                        color: bal >= 20 ? '#36a64f' : bal >= 1 ? '#FECC43' : '#FF0000',
-                        fields: trans.reverse().map(t => ({
-                            short: false,
-                            title: t.description,
-                            value: `- $${t.price.toFixed(2)}`
-                        }))
-                    }
-                ]
-            });
+    budget.balance(userId, (err, {balance}) =>  {
+        b.reply(m, `Your balance is *$${balance}*`);
     });
-}
+};
 
 module.exports = controller => {
     controller.on('direct_message,direct_mention,ambient', (b, m) => {
@@ -34,13 +19,14 @@ module.exports = controller => {
             if (/^(budget )?balance/i.test(action)) {
                 respondWithBalance(b, m, userId);
             } else if (/^budget /i.test(action)) {
-                budget.bought(userId, ...action.substr(7).split(','))
-                    .then(({bal}) => reply(b, m, `You have $${bal} left`))
-                    .catch(err => reply(b, m, err));
+                const [ price, description ] = action.substr(7).split(',');
+                budget.bought(userId, price, description, (err, {balance}) => {
+                    reply(b, m, err || `You have $${balance} left`);
+                });
             }  else if (isInPaycheckChannel && /^reset /i.test(action)) {
-                budget.onPaycheckReset()
-                    .then(() => reply(b, m, `Budget reset... calculating balance`))
-                    .then(() => respondWithBalance(b, m, userId));
+                // budget.onPaycheckReset()
+                //     .then(() => reply(b, m, `Budget reset... calculating balance`))
+                //     .then(() => respondWithBalance(b, m, userId));
             }
         }
     });
