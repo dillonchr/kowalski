81ddfba8c127810626189cae819ce93e4786cb46
Dillon Christensen <dillon.christensen@kalibrate.com>
2019-03-26 12:32:05 -0500

    made the inflation calc work like flexo, accepts the

--- handlers/inflation.js
+++ handlers/inflation.js
@@ -2,7 +2,7 @@ const {inflation} = require('@dillonchr/funhouse');
 
 module.exports = bot => {
     bot.hears(['how much was '], (reply, {content}) => {
-        const matches = content.match(/\$([\d\.]+) in (\d{4})$/i);
+        const matches = content.match(/\$([\d\.]+) in (\d{4})\??$/i);
 
         if (matches && matches.length === 3) {
             const [dollars, year] = matches.slice(1);
@@ -13,7 +13,7 @@ module.exports = bot => {
 
             inflation(dollars, year, (err, data) => {
                 if (data && data.valueThen) {
-                    reply(`$${data.valueNow} was worth **$${data.valueThen}** in ${data.year}`);
+                    reply(`$${data.valueNow} was worth **${data.valueThen}** in ${data.year}`);
                 } else {
                     reply(`I don't know what the rate was for ${year}.`);
                 }
