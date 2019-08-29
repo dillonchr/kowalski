06acbcbd03acb012b104ef2b833443b1cf3c60ec
Dillon Christensen <j.dillon.christensen@gmail.com>
2019-08-28 20:48:22 -0700

    fixed gg crissy field cam

--- handlers/sf/gg.js
+++ handlers/sf/gg.js
@@ -1,7 +1,34 @@
+const fetch = require('@dillonchr/fetch');
+
+const getCrissyFieldCam = (done) => {
+    fetch({
+        url: `http://173.164.254.148/ptz.cgi?doc=East%20Beach%20Webcam&xml=1&cmd=open&version=20100917&kind=ctl`
+    }, (err, body) => {
+        if (err) {
+            done(err);
+        } else {
+            try {
+            const [ignore, id] = body.match(/key="id" value="([^"]+)"/);
+            done(null, `http://173.164.254.148/vid.cgi?id=${id}&doc=East%20Beach%20Webcam&i=1&r=${Math.random()}`);
+            } catch(err) {
+                done(err);
+            }
+        }
+    });
+};
+
+
 module.exports = (bot) => {
     bot.hears(['gg','golden gate'], ({reply}) => {
         const cc = new Date().getTime();
-        reply(`https://media.kron.com/nxs-krontv-media-us-east-1/Traffic/cam3_2_large.jpg?cc=${cc}\nhttp://173.164.254.148/vid.cgi?id=S44197_BYLD&doc=East%20Beach%20Webcam&i=1&r=0.6992534210555819`);
+        reply(`https://media.kron.com/nxs-krontv-media-us-east-1/Traffic/cam3_2_large.jpg?cc=${cc}`);
+        getCrissyFieldCam((err, url) => {
+            if (err) {
+                console.log('CrissyFieldErr:', err);
+            } else {
+                reply(url);
+            }
+        });
     });
 };
 
