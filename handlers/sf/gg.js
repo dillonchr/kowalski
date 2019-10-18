9f2ce037a502dd162d295d2dcce5fc0f92e5a040
Dillon Christensen <dillon@goloansnap.com>
2019-10-18 00:59:16 -0500

    upgraded gg to be live gif

--- handlers/sf/gg.js
+++ handlers/sf/gg.js
@@ -1,32 +1,14 @@
-const fetch = require('@dillonchr/fetch');
-
-const getCrissyFieldCam = (done) => {
-    fetch({
-        url: `http://173.164.254.148/ptz.cgi?doc=East%20Beach%20Webcam&xml=1&cmd=open&version=20100917&kind=ctl`
-    }, (err, body) => {
-        if (err) {
-            done(err);
-        } else {
-            try {
-            const [ignore, id] = body.match(/key="id" value="([^"]+)"/);
-            done(null, `http://173.164.254.148/vid.cgi?id=${id}&doc=East%20Beach%20Webcam&i=1&r=${Math.random()}`);
-            } catch(err) {
-                done(err);
-            }
-        }
-    });
-};
-
+const { exec } = require('child_process');
 
 module.exports = (bot) => {
     bot.hears(['gg','golden gate'], ({reply}) => {
-        const cc = new Date().getTime();
-        reply(`https://media.kron.com/nxs-krontv-media-us-east-1/Traffic/cam3_2_large.jpg?cc=${cc}`);
-        getCrissyFieldCam((err, url) => {
-            if (err) {
-                console.log('CrissyFieldErr:', err);
+        const kron = `https://media.kron.com/nxs-krontv-media-us-east-1/Traffic/cam3_2_large.jpg?cc=${new Date().getTime()}`
+        exec('../../make.sh', {cwd: __dirname, shell: true}, (err, o, stdErr) => {
+            const gifPath = o && !err && o.trim()
+            if (o && o.trim().match(/\.gif$/)) {
+                reply(kron, {files: [o.trim()]});
             } else {
-                reply(url);
+                reply(kron);
             }
         });
     });
