635868fc16c48cbae4f04b379c836bb79e7999a8
Dillon Christensen <j.dillon.christensen@gmail.com>
2020-06-16 15:28:22 -0500

    checking in changes for a bit

--- handlers/paycheck.js
+++ handlers/paycheck.js
@@ -1,56 +1,72 @@
-const {paycheck} = require('@dillonchr/funhouse');
-const {trackError} = require('../utils');
+const { paycheck } = require("@dillonchr/funhouse");
+const moment = require("moment");
+const { trackError } = require("../utils");
 const is = {
-    balance: s => /^balance/i.test(s),
-    help: s => /^help$/i.test(s),
-    debit: s => /([\d.-]+),(.*)$/.test(s),
-    budget: s => /^budget /i.test(s),
-    reset: s => /^reset /i.test(s)
+  balance: s => /^balance/i.test(s),
+  help: s => /^help$/i.test(s),
+  debit: s => /([\d.-]+),(.*)$/.test(s),
+  budget: s => /^budget /i.test(s),
+  reset: s => /^reset /i.test(s)
 };
 
 module.exports = bot => {
-    bot.hearsAnythingInChannel(process.env.PAYCHECK_CHANNEL_ID, ({reply, content}) => {
-        const action = content.trim();
+  bot.hearsAnythingInChannel(
+    process.env.PAYCHECK_CHANNEL_ID,
+    ({ reply, channel, content }) => {
+      const action = content.trim();
 
-        if (is.balance(action)) {
-            paycheck.balance((err, bal) => {
-                if (err) {
-                    trackError(err);
-                    reply(`Probalo! ${err.message}`);
-                } else {
-                    reply(`You have $${bal.balance}`);
-                }
-            });
-        } else if (is.debit(action) && !is.budget(action)) {
-            try {
-                const [ignore, price] = action.match(/([\d.-]+),(.*)$/);
+      if (is.balance(action)) {
+        paycheck.balance((err, bal) => {
+          if (err) {
+            trackError(err);
+            reply(`Probalo! ${err.message}`);
+          } else {
+            reply(`You have $${bal.balance}`);
+          }
+        });
+      } else if (is.debit(action) && !is.budget(action)) {
+        try {
+          const [ignore, price] = action.match(/([\d.-]+),(.*)$/);
 
-                if (isNaN(price)) {
-                    return reply(`\`${price}\` isn\'t a proper amount.`);
-                }
+          if (isNaN(price)) {
+            return reply(`\`${price}\` isn\'t a proper amount.`);
+          }
 
-                paycheck.spend(price, (err, result) => {
-                    if (err) {
-                        trackError(err);
-                        reply(`Paycheck error: ${err.message}`);
-                    } else {
-                        reply(`Paycheck balance: $${result.balance}`);
-                    }
+          paycheck.spend(price, async (err, result) => {
+            if (err) {
+              trackError(err);
+              reply(`Paycheck error: ${err.message}`);
+            } else {
+              try {
+                await channel.edit({
+                  topic: `$${result.balance} as of ${moment().format(
+                    "h:mma ddd"
+                  )}`
                 });
-            } catch (err) {
-                trackError(err);
-                reply(`Paycheck debit error: ${err.message}`);
-            }
-        } else if (is.reset(action)) {
-            paycheck.reset(action.substr(5).trim(), (err, result) => {
-                if (err) {
-                    trackError(err);
-                    reply(`Paycheck error: ${err.message}`);
-                } else {
-                    reply(`Paycheck balance reset to $${result.balance} :+1:`);
+              } catch (err) {
+                if (/Missing Permissions/i.test(err.message)) {
+                  console.log(
+                    "Need to give 'Modify Channel' permissions to Kowalski"
+                  );
                 }
-            });
+                reply(`Paycheck balance: $${result.balance}`);
+              }
+            }
+          });
+        } catch (err) {
+          trackError(err);
+          reply(`Paycheck debit error: ${err.message}`);
         }
-    });
+      } else if (is.reset(action)) {
+        paycheck.reset(action.substr(5).trim(), (err, result) => {
+          if (err) {
+            trackError(err);
+            reply(`Paycheck error: ${err.message}`);
+          } else {
+            reply(`Paycheck balance reset to $${result.balance} :+1:`);
+          }
+        });
+      }
+    }
+  );
 };
-
--- handlers/sf/gg.js
+++ handlers/sf/gg.js
@@ -1,16 +1,31 @@
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 
-module.exports = (bot) => {
-    bot.hears(['gg','golden gate'], ({reply}) => {
-        const kron = `https://media.kron.com/nxs-krontv-media-us-east-1/Traffic/cam3_2_large.jpg?cc=${new Date().getTime()}`
-        exec('../../make.sh', {cwd: __dirname, shell: true}, (err, o, stdErr) => {
-            const gifPath = o && !err && o.trim()
-            if (o && o.trim().match(/\.gif$/)) {
-                reply(kron, {files: [o.trim()]});
-            } else {
-                reply(kron);
-            }
-        });
+const emoji = () => (Date.now() % 2 === 0 ? ":foggy:" : ":bridge_at_night:");
+
+const GG_FRAME_COUNT = 10;
+
+module.exports = bot => {
+  bot.hears(["gg", "golden gate"], ({ reply }) => {
+    const gifTimerId = setTimeout(
+      () =>
+        reply(
+          "Wasn't able to build your gif, bud.\nSorry about that.\n" + emoji()
+        ),
+      GG_FRAME_COUNT * 1000
+    );
+    exec("../../make.sh", { cwd: __dirname, shell: true }, (err, o, stdErr) => {
+      const gifPath = o && !err && o.trim();
+      clearTimeout(gifTimerId);
+      if (o && o.trim().match(/\.gif$/)) {
+        reply(emoji() + " -> " + o.trim(), { files: [o.trim()] });
+      } else {
+        reply(`Couldn't build your gif!
+        ${emoji()}
+        \`\`\`
+        ${stdErr}
+        ${o}
+        \`\`\``);
+      }
     });
+  });
 };
-
