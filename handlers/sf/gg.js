ddd2117b445f4418b8d1d29275ad01394779bdcd
Dillon Christensen <j.dillon.christensen@gmail.com>
2021-11-26 09:32:59 -0600

    patched overzealous `gg` handling #36 I was tried of seeing that stupid thing pop up that it couldn't create my gif whenever I type something into paycheck so I finally fixed it to verify it's a direct message. But also gg is working locally. So maybe a reboot is in order?

--- handlers/sf/gg.js
+++ handlers/sf/gg.js
@@ -6,39 +6,41 @@ const GG_FRAME_COUNT = 10;
 
 module.exports = bot => {
   bot.hears(["gg", "golden gate"], async message => {
-    const { reply } = message;
-    await message.react("⏳");
+    if (null != message.channel && "dm" === message.channel.type) {
+      const { reply } = message;
+      await message.react("⏳");
 
-    const gifTimerId = setTimeout(
-      () =>
-        reply(
-          "Wasn't able to build your gif, bud.\nSorry about that.\n" + emoji()
-        ),
-      (GG_FRAME_COUNT + 1) * 1000
-    );
-    exec(
-      "../../make.sh",
-      { cwd: __dirname, shell: true },
-      async (err, o, stdErr) => {
-        const gifPath = o && !err && o.trim();
-        clearTimeout(gifTimerId);
-        if (gifPath && gifPath.match(/\.gif$/)) {
-          await reply(emoji(), {
-            files: [
-              {
-                attachment: gifPath
-              }
-            ]
-          });
-        } else {
-          reply(`Couldn't build your gif!
+      const gifTimerId = setTimeout(
+        () =>
+          reply(
+            "Wasn't able to build your gif, bud.\nSorry about that.\n" + emoji()
+          ),
+        (GG_FRAME_COUNT + 1) * 1000
+      );
+      exec(
+        "../../make.sh",
+        { cwd: __dirname, shell: true },
+        async (err, o, stdErr) => {
+          const gifPath = o && !err && o.trim();
+          clearTimeout(gifTimerId);
+          if (gifPath && gifPath.match(/\.gif$/)) {
+            await reply(emoji(), {
+              files: [
+                {
+                  attachment: gifPath
+                }
+              ]
+            });
+          } else {
+            reply(`Couldn't build your gif!
         ${emoji()}
         \`\`\`
         ${stdErr}
         ${o}
         \`\`\``);
+          }
         }
-      }
-    );
+      );
+    }
   });
 };
