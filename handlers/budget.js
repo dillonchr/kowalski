d0f316e910fa8f2b2ad09a81e74caa9a99c63fa6
Dillon Christensen <j.dillon.christensen@gmail.com>
2024-04-01 11:40:47 -0500

    only show budget bal when asking

--- handlers/budget.js
+++ handlers/budget.js
@@ -11,25 +11,22 @@ const respondWithBalance = (reply, userId) => {
   });
 };
 
-module.exports = bot => {
-  bot.hearsAnythingInChannel(
-    process.env.PAYCHECK_CHANNEL_ID,
-    ({ reply, content, author }) => {
-      const action = content.trim();
-      const userId = author.id;
+module.exports = (bot) => {
+  bot.hearsAnythingInChannel(process.env.PAYCHECK_CHANNEL_ID, ({ reply, content, author }) => {
+    const action = content.trim();
+    const userId = author.id;
 
-      if (/^(budget )?balance/i.test(action)) {
-        respondWithBalance(reply, userId);
-      } else if (/^budget /i.test(action)) {
-        const [amount, description] = action.substr(7).split(",");
-        budget.bought(userId, { amount, description }, (err, data) => {
-          if (err) {
-            trackError(err);
-            return reply(`Budget broke: ${err.message}`);
-          }
-          reply(`@${author.username}: $${data.balance}`);
-        });
-      }
+    if (/^budget balance/i.test(action)) {
+      respondWithBalance(reply, userId);
+    } else if (/^budget /i.test(action)) {
+      const [amount, description] = action.substr(7).split(",");
+      budget.bought(userId, { amount, description }, (err, data) => {
+        if (err) {
+          trackError(err);
+          return reply(`Budget broke: ${err.message}`);
+        }
+        reply(`@${author.username}: $${data.balance}`);
+      });
     }
-  );
+  });
 };
