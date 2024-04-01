1e39b9294efa2205230e69f9d9a8708a7b10b685
Dillon Christensen <j.dillon.christensen@gmail.com>
2024-04-01 11:36:38 -0500

    maybe this is good

--- handlers/paycheck.js
+++ handlers/paycheck.js
@@ -2,44 +2,32 @@ const { paycheck } = require("@dillonchr/funhouse");
 const moment = require("moment");
 const { trackError } = require("../utils");
 const is = {
-  balance: s => /^balance/i.test(s),
-  help: s => /^help$/i.test(s),
-  debit: s => /([\d.-]+),(.*)$/.test(s),
-  budget: s => /^budget /i.test(s),
-  reset: s => /^reset /i.test(s)
+  balance: (s) => /^balance/i.test(s),
+  help: (s) => /^help$/i.test(s),
+  debit: (s) => /([\d.-]+),(.*)$/.test(s),
+  budget: (s) => /^budget /i.test(s),
+  reset: (s) => /^reset /i.test(s),
 };
 
-const EMOJIS = [
-  "🤑",
-  "👑",
-  "💸",
-  "💵",
-  "💰",
-  "💳",
-  "⚖️",
-  "🌼",
-  "💶",
-  "🥇",
-  "🌝"
-];
+const EMOJIS = ["🤑", "👑", "💸", "💵", "💰", "💳", "⚖️", "🌼", "💶", "🥇", "🌝"];
 const emote = () => EMOJIS[~~(Math.random() * EMOJIS.length)];
 
-module.exports = bot => {
-  bot.hearsAnythingInChannel(process.env.PAYCHECK_CHANNEL_ID, async message => {
+module.exports = (bot) => {
+  bot.hearsAnythingInChannel(process.env.PAYCHECK_CHANNEL_ID, async (message) => {
     const action = message.content.trim();
     const { reply } = message;
 
     function jazzedUpReply(replyStr) {
-      reply(replyStr.replace(/\$100.00$/, "💯").replace(/\$-?0\.00$/, "$0 💀"));
+      return reply(replyStr.replace(/\$100.00$/, "💯").replace(/\$-?0\.00$/, "$0 💀"));
     }
 
-    if (is.balance(action)) {
-      paycheck.balance((err, bal) => {
+    if (is.balance(action) && !is.budget(action)) {
+      paycheck.balance(async (err, bal) => {
         if (err) {
           trackError(err);
-          reply(`Probalo! ${err.message}`);
+          await reply(`Probalo! ${err.message}`);
         } else {
-          jazzedUpReply(`You have $${bal.balance}`);
+          await jazzedUpReply(`You have $${bal.balance}`);
         }
       });
     } else if (is.debit(action) && !is.budget(action)) {
@@ -47,50 +35,41 @@ module.exports = bot => {
         const [ignore, price] = action.match(/([\d.-]+),(.*)$/);
 
         if (isNaN(price)) {
-          return reply(`\`${price}\` isn\'t a proper amount.`);
+          return await reply(`\`${price}\` isn\'t a proper amount.`);
         }
-
-        message.react("👌");
 
         paycheck.spend(price, async (err, result) => {
           if (err) {
             trackError(err);
-            reply(`Paycheck error: ${err.message}`);
+            await reply(`Paycheck error: ${err.message}`);
           } else {
             try {
-              jazzedUpReply(`${emote()} $${result.balance}`);
-              await message.channel.edit({
-                topic: `$${result.balance} as of ${moment().format(
-                  "h:mma ddd"
-                )}`
-              });
+              await jazzedUpReply(`${emote()} $${result.balance}`);
             } catch (err) {
               if (/Missing Permissions/i.test(err.message)) {
-                console.log(
-                  "Need to give 'Modify Channel' permissions to Kowalski"
-                );
+                console.log("Need to give 'Modify Channel' permissions to Kowalski");
               }
             }
           }
         });
       } catch (err) {
         trackError(err);
-        reply(`Paycheck debit error: ${err.message}`);
+        await reply(`Paycheck debit error: ${err.message}`);
       }
     } else if (is.reset(action)) {
-      paycheck.balance((err, bal) => {
+      paycheck.balance(async (err, bal) => {
         if (err) {
-          reply("Oops");
+          await reply("Oops");
           trackError(err);
         } else {
           const leftovers = bal.balance;
 
-          paycheck.reset(action.substr(5).trim(), (err, result) => {
+          paycheck.reset(action.substr(5).trim(), async (err, result) => {
             if (err) {
               trackError(err);
-              reply(`Paycheck error: ${err.message}`);
+              await reply(`Paycheck error: ${err.message}`);
             } else {
-              reply(`Paycheck balance reset to $${result.balance} :+1:`);
+              await reply(`Paycheck balance reset to $${result.balance} :+1:`);
             }
           });
         }
--- make.sh
+++ make.sh
@@ -4,7 +4,7 @@ if [ ! -d "$LOCATION" ]; then
   mkdir -p "$LOCATION"
 fi
 
-ID="$(curl -s http://173.164.254.148/ptz.cgi\?doc\=East%20Beach%20Webcam\&xml\=1\&cmd\=open\&version\=20100917\&kind\=ctl | egrep -o 'key="id" value="([A-Z0-9_]+)"' | sed -n -E 's/.*value="([^"]+)"/\1/p')"
+ID="$(curl -s https://parksconservancy.siteproxy.net/api/session\?doc\=East+Beach+Webcam\&kind\=ctl\&mode\=MJPEG\&stream\=East_Beach_Webcam_MJPEG_640x480\&msg\=1\&cmd\=open | egrep -o 'key="id" value="([A-Z0-9_]+)"' | sed -n -E 's/.*value="([^"]+)"/\1/p')"
 URL="http://173.164.254.148/vid.cgi?id=${ID}&doc=East%20Beach%20Webcam&i=1"
 
 if [ "$?" -eq "0" ]; then
